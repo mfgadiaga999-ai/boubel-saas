66bc8e5981f733c11e32623ce85e4951cd1be0f2
Fallou GADIAGA <mfgadiaga999@gmail.com>
2026-08-17 22:01:49 +0000

    Ajout mise à jour de la partie facturation des produits et maintenance

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 import os
 from collections import Counter
 from datetime import datetime
+from datetime import date
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from supabase import create_client, Client
